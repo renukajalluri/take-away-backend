1875ea7af0e0b295c6192ed34797a17a34993345
renukajalluri <renukajalluri20@gmail.com>
2023-01-22 19:35:48 +0530

    port_env

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const app = require('./app') // the actual Express application
-const http = require('http')
-const config = require('./utils/config')
+const app = require("./app"); // the actual Express application
+const http = require("http");
+const config = require("./utils/config");
+
 // const logger = require('./utils/logger')
+const port = config.PORT || 3000;
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-server.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
-})
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
